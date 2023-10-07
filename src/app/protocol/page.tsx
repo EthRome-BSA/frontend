73624e1feff90e2d38424af88aa436aca1af15ae
Labdhi Jain <labdhijain@Labdhis-MacBook-Pro.local>
2023-10-07 13:35:03 -0500

    made 2 pages + css

---
+++ src/app/protocol/page.tsx
@@ -0,0 +1,22 @@
+
+import styles from './../page.module.css'
+
+export default function Page() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <h>
+          Reviews will come here
+        </h>
+        <div>
+         
+        </div>
+      </div>
+
+
+     
+    
+    
+    </main>
+  )
+}
