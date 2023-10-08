3b1a1c436aacff5dbb5186ec99a41b084e556cd2
Labdhi Jain <labdhijain@Labdhis-MacBook-Pro.local>
2023-10-07 22:16:22 -0500

    basic UI working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,64 +3,53 @@ import styles from './page.module.css'
 import Link from 'next/link';
 
 export default function Page() {
+  let protocols = [{name: "Uniswap", description:"Description"}, 
+                  {name: "Aave", description:"Description"},
+                  {name: "Protocol 3", description:"Description"},
+                  {name: "Protocol 4", description:"Description"},
+                  {name: "Protocol 5", description:"Description"}]
+
   return (
     <main className={styles.main}>
       <div className={styles.center}>
         <h1 className={styles.title}>
           RatingFinance
         </h1>
-        <div>
-          
-        </div>
       </div>
 
       <div className={styles.center}>
+      <h3 className={styles.description}>
+          [insert small paragraph about what RatingFinance is]
+      </h3>
+      </div>
+<div className={styles.card1}>
+<div className={styles.center}>
+      <h2>
+          Click on a Protocol to read verified reviews, or to submit a review:
+      </h2>
+      </div>
       <div className={styles.grid}>
-        <a
-          href="/protocol"
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Uniswap <span>-&gt;</span>
-          </h2>
-          <p>Description</p>
-        </a>
+      
+      
+      {protocols.map(ptcl => (
+         <Link
+         href={{ pathname: '/protocol', query: { name: ptcl.name } }}
+         className={styles.card}
+         rel="noopener noreferrer"
+       >
+         <h2>
+           {ptcl.name} <span>-&gt;</span>
+         </h2>
+         <p>{ptcl.description}</p>
+       </Link>
+      ))}
+       </div>
 
-        <a
-          href="/protocol"
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Uniswap <span>-&gt;</span>
-          </h2>
-          <p>Description</p>
-        </a>
-
-        <a
-          href="/protocol"
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Uniswap <span>-&gt;</span>
-          </h2>
-          <p>Description</p>
-        </a>
-
-        <a
-          href="/protocol"
-          className={styles.card}
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Uniswap <span>-&gt;</span>
-          </h2>
-          <p>Description</p>
-        </a>
-      </div>
-      </div>
+</div>
+      
+        
+     
+     
 
       
     </main>
--- src/app/protocol/page.tsx
+++ src/app/protocol/page.tsx
@@ -1,22 +1,112 @@
+"use client";
+import { useSearchParams } from 'next/navigation'
+import styles from '../page.module.css'
+import ReactStars from 'react-stars'
+import {useState} from "react";
+import Modal from 'react-modal';
 
-import styles from './../page.module.css'
+export default function Page(){
+  const searchParams = useSearchParams()
+  const [showModal, setShowModal] = useState(false);
+ 
+  const name = searchParams.get('name')
+  let reviews = [{review: "Uniswap is gas Uniswap is gas Uniswap is gas Uniswap is gas Uniswap is gas Uniswap is gas  Uniswap is gas"},
+                {review: "Uniswap is aweful"},
+                { review: "Uniswap is a mediumly annoying protocol"}]
+  let stars = 4.2;
 
-export default function Page() {
+
+  
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
   return (
     <main className={styles.main}>
+
+
       <div className={styles.center}>
-        <h>
-          Reviews will come here
-        </h>
         <div>
-         
-        </div>
+        <h1 className={styles.title}>
+          {name}
+        </h1>
+
+        <ReactStars 
+        count={5} 
+        size={30} 
+        color2={'#ff0077'}
+        value={stars}
+        edit={false}
+         /> 
+         <p>{stars}</p>
+          </div>
+     
+      </div>
+
+    
+
+          <button className={styles.button} onClick={() => setShowModal(true)}> Submit a Review</button>
+
+          <Modal
+            isOpen={showModal}
+            // onRequestClose={setShowModal}
+            style={styles.main}
+            contentLabel="Example Modal"
+          >
+            <main className={styles.main}>
+              <h2>Hello</h2>
+              <button onClick={() => setShowModal(false)}>close</button>
+              <div>I am a modal</div>
+              <form>
+                <input />
+                <button>tab navigation</button>
+                <button>stays</button>
+                <button>inside</button>
+                <button>the modal</button>
+              </form>
+            </main>
+            
+          </Modal>
+
+      <div className={styles.card1}>
+<div className={styles.center}>
+      <h2>
+          Read the reviews of the {name} protocol users:
+      </h2>
+      </div>
+
+        <div className={styles.grid}>
+              
+              
+              {reviews.map(rvw => (
+                <div
+                className={styles.card1}
+                rel="noopener noreferrer"
+              >
+                
+                <p>{rvw.review}</p>
+              </div>
+              ))}
+                
+              </div>
       </div>
 
 
-     
-    
-    
+
+      
+
+
+
+      
     </main>
-  )
-}
+  );
+};
+
